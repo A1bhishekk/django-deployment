1eac54e1817d2026fb80bd519308c2547f8bcbcb
Technical Abhi <72288986+A1bhishekk@users.noreply.github.com>
2024-06-20 16:46:11 +0530

    f

--- abhishek/abhishek/settings.py
+++ abhishek/abhishek/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2p#vo8#cad54rjkxt8o#hpqnxnhxqxkf@+28kn%^7-5mj6d3))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-deployment-f1zs.onrender.com']
 
